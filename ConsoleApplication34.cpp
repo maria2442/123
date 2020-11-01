c8015b38d5666eef0342143c7d58a8d7e1ab60c1
maria2442 <mariat04@yandex.ru>
2020-11-02 01:54:18 +0300

    number1

---
+++ ConsoleApplication34.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double a, b, k, n,m,l,j;
+	cin >> a >> b;
+	m = (2 * a - b) / 3;
+	n = (2 * b - a) / 3;
+	if ((m - int(m) == 0) && (n - int(n) == 0))
+		cout << int(m) << " " << int(n);
+	else
+		cout << "-1";
+
+	
+}
+
